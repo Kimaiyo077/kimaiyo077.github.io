6e78371ea74f297d65d03e153e8a1659e8053437
Kimaiyo077 <biwot777@gmail.com>
2022-02-15 15:59:29 +0300

    Updates

---
+++ precache-manifest.7342f18598deb02d3cddd9b35e504540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50ea6bf2eb9f910f61ad7df42c960f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71237b9f02afcdbb1abb",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0291e87369d459427d8f",
+    "url": "/static/css/main.28647a25.chunk.css"
+  },
+  {
+    "revision": "71237b9f02afcdbb1abb",
+    "url": "/static/js/2.cabedc0f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.cabedc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0291e87369d459427d8f",
+    "url": "/static/js/main.38167f9a.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
